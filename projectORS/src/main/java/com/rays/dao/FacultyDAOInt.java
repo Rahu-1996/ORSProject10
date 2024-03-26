67eafad5ed8a5415d23645f395140bec5f8f9689
Rahu-1996 <choudhary.rahul4796@gmail.com>
2024-03-26 22:24:45 +0530

    done

---
+++ projectORS/src/main/java/com/rays/dao/FacultyDAOInt.java
@@ -0,0 +1,11 @@
+package com.rays.dao;
+
+import com.rays.common.BaseDAOInt;
+import com.rays.dto.FacultyDTO;
+/**
+ *RAHUL CHOUDHARY
+ *
+ */
+public interface FacultyDAOInt extends BaseDAOInt<FacultyDTO> {
+
+}
